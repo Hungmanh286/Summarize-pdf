4ade1c99e6dc6e5ffae08566da2fcf1a5ffc9f51
hungmanh286 <hungmanh99992k4@gmailcom>
2025-02-17 11:29:26 +0700

    update

--- src/summarization.py
+++ src/summarization.py
@@ -1,7 +1,14 @@
 import google.generativeai as genai
+import os
+from dotenv import load_dotenv
 
 
-genai.configure(api_key="YOUR_GEMINI_API_KEY")
+load_dotenv()
+
+
+API_KEY = os.getenv("API_KEY")
+
+genai.configure(api_key=API_KEY)
 
 def summarize_with_llm(text):
     model = genai.GenerativeModel("gemini-pro")
